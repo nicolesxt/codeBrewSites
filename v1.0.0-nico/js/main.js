c78c0ad0e0b14206a62201d8b5d8d7897bde8987
NicoSxt <123sxtnicole@gmail.com>
2015-11-17 11:52:55 -0500

    this is a repo for code brew website designs
    This is a repo for code brew website designs

---
+++ v1.0.0-nico/js/main.js
@@ -0,0 +1,21 @@
+$(".menu").hide();
+
+$(".menuicon").hover(function(){
+	$(".menu").show();
+    }, function(){
+    $(".menu").hide();
+});
+
+$(".menu").hover(function(){
+	$(".menu").show();
+    }, function(){
+    $(".menu").hide();
+});
+
+$(".menulist").hover(function(){
+	$(".menu").show();
+    }, function(){
+    $(".menu").hide();
+});
+
+console.log('fdsafsad');
